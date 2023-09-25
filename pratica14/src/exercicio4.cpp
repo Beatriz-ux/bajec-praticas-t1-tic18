2f164419e0950dde0cba1205531805bbd66117fe
albertolunia <albertolunia@gmail.com>
2023-09-25 08:50:16 -0300

    Fix exer4, criando exer5

--- pratica14/src/exercicio4.cpp
+++ pratica14/src/exercicio4.cpp
@@ -5,19 +5,6 @@ using namespace std;
 double areaQuadrado(double lado);
 double perimetroQuadrado(double lado);
 
-int main(){
-
-    double lado;
-
-    cout << "Digite o lado do quadrado: ";
-    cin >> lado;
-
-    cout << "Area: " << areaQuadrado(lado) << endl;
-    cout << "Perimetro: " << perimetroQuadrado(lado) << endl;
-
-    return 0;
-}
-
 double areaQuadrado(double lado) {
     return lado * lado;
 }
---
+++ pratica14/src/exercicio5.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+struct aluno{
+    string nome;
+    string sobrenome;
+    int idade;
+    int numMatricula;
+    string curso;
+};
