1c7f1eca04e4bf1aa5f2a89da91aa9bd911763f1
Everaldina G. Barbosa <everaldinag@gmail.com>
2023-09-27 11:20:49 -0300

    fix: edicao no menu

--- pratica19/src/exercicio1.cpp
+++ pratica19/src/exercicio1.cpp
@@ -44,29 +44,48 @@ int main(){
     int opcao;
     ItemSet A;
     ItemSet B;
+    ItemSet C;
     
     B.inserir("item1");
     B.inserir("item2");
 
-    cout << "B: ";
-    B.printItens();
 
-    cout << "Insira sua operação: " << endl;
-    cout << "1 - A = B" << endl;
+
+    cout << "\n1 - A = B" << endl;
     cout << "2 - A = B + C" << endl;
     cout << "3 - A = B * C" << endl;
     cout << "4 - A = B - C" << endl;
     cout << "5 - A = B <> C" << endl;
     cout << "6 - A = B == C" << endl;
+    cout << "0 - Sair" << endl;
+    cout << "Opção: ";
     cin >> opcao;
 
     switch (opcao)
     {
-    case 1:
-        A = B;
-        cout << "A: ";
-        A.printItens();
-        break;
+        case 0:
+            break;
+        case 1: // A = B
+            cout << "B: ";
+            B.printItens();
+            
+            A = B;
+            cout << "A: ";
+            A.printItens();
+            break;
+        case 2: // A = B + C
+            break;
+        case 3: // A = B * C
+            break;
+        case 4: // A = B - C
+            break;
+        case 5: // A = B <> C
+            break;
+        case 6: // A = B == C
+            break;
+        default:
+            cout << "Opção inválida" << endl;
+            break;
     };
     return 0;
 }
