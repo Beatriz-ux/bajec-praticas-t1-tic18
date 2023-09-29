562115039101004c66e3b19969c056bad3256be1
Everaldina G. Barbosa <everaldinag@gmail.com>
2023-09-28 22:52:30 -0300

    feat: comentarios e testes operdares - e ==

--- pratica19/src/exercicio1.cpp
+++ pratica19/src/exercicio1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <iomanip>
 
 using namespace std;
 
@@ -51,8 +52,9 @@ class ItemSet{
         }
 
         ItemSet operator-(ItemSet itemSetB){
-            ItemSet result;
+            ItemSet result; // conjunto resultante
 
+            // busca cada item de A em B, se nao encontrar, insere no resultado
             for(string sA : itens){
                 if(!itemSetB.buscar(sA)){
                     result.inserir(sA);
@@ -61,10 +63,13 @@ class ItemSet{
             return result;
         }
 
-        bool operator==(ItemSet itemSetB){
+        // nao verifica ordem apenas se os itens sao os mesmos
+        bool operator==(const ItemSet &itemSetB){
+            // se a quantidade de itens for diferente, nao sao iguais
             if(this->itens.size() != itemSetB.itens.size())
                 return false;
 
+            // buscas cada item de B no objeto atual
             for(string sB : itemSetB.itens){
                 if(!buscar(sB))
                     return false;
@@ -80,6 +85,8 @@ int main(){
     ItemSet A;
     ItemSet B;
     ItemSet C;
+    ItemSet D;
+    ItemSet E;
     
     B.inserir("item1");
     B.inserir("item2");
@@ -89,13 +96,20 @@ int main(){
     C.inserir("item3");
     C.inserir("item4");
 
+    D.inserir("item2");
+    D.inserir("item1");
+
+    E.inserir("item1");
+    E.inserir("item3");
+    E.inserir("item2");
+
 
     cout << "\n1 - A = B" << endl;
     cout << "2 - A = B + C" << endl;
     cout << "3 - A = B * C" << endl;
-    cout << "4 - A = B - C" << endl;
+    cout << "4 - Operador - " << endl;
     cout << "5 - A = B <> C" << endl;
-    cout << "6 - B == C" << endl;
+    cout << "6 - Operador == " << endl;
     cout << "0 - Sair" << endl;
     cout << "Opção: ";
     cin >> opcao;
@@ -117,24 +131,44 @@ int main(){
         case 3: // A = B * C
             break;
         case 4: // A = B - C
+            cout << "\n(A - B) retorna um novo objeto ItemSet com os itens de A que nao estao em B.\n";
+            cout << "Conjuntos: \n";
             cout << "B: ";
             B.printItens();
             cout << "C: ";
             C.printItens();
-            cout << "B - C: ";
+            cout << "\nB - C: ";
             //A = B - C;
             (B - C).printItens();
+            cout << "C - B: ";
+            //A = C - D;
+            (C - B).printItens();
             break;
         case 5: // A = B <> C
             break;
         case 6: // A = B == C
+            cout << boolalpha;
+            cout << "\n(A == B) retorna true se os itens de A forem iguais aos itens de B. Nao verifica ordem de itens.\n";
+            
+            // prints dos conjuntos
+            cout << "Conjuntos: \n";
             cout << "B: ";
             B.printItens();
             cout << "C: ";
             C.printItens();
-            cout << "B - C: ";
-            //A = B - C;
-            (B - C).printItens();
+            cout << "D:";
+            D.printItens();
+            cout << "E:";
+            E.printItens();
+
+            // comparacoes
+            cout << "\nB == B: " << (B == B) << endl;
+            cout << "B == C: " << (B == C) << endl;
+            cout << "C == B: " << (C == B) << endl;
+            cout << "B == D: " << (B == D) << endl;
+            cout << "D == B: " << (D == B) << endl;
+            cout << "B == E: " << (B == E) << endl;
+            cout << "E == B: " << (E == B) << endl;
             break;
         default:
             cout << "Opção inválida" << endl;
